2d5bb190ca7b82116908b92ef33cfc30fa87ea51
Sankalp Bhatt <glitter.sankalp@gmail.com>
2018-04-25 08:41:03 +0530

    Add Mget for redis cluster

--- adapters/redis/redis_cluster_adapter.go
+++ adapters/redis/redis_cluster_adapter.go
@@ -1,6 +1,9 @@
 package redis
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+	"github.com/joaojeronimo/go-crc16"
+)
 
 const SLOTS_COUNT = 16384
 
@@ -17,4 +20,48 @@ func (this *RedisCluster) Initialize(config RedisClusterConfig) {
 		Addrs: config.Addrs,
 	})
 	this.Client = client
+}
+
+// Override base redis implementation of Mget.
+// @done: - Group keys based on the slots they belong.
+// @todo: - Make concurrent calls to attain parallelism.
+func (this *RedisCluster) MGet(a ...string) (map[string][]byte, error) {
+
+	if len(a) == 0 {
+		return nil, nil
+	}
+	//slotify
+	m := make(map[uint16][]string)
+	for _, key := range a {
+		slot := (crc16.Crc16([]byte(key))) % SLOTS_COUNT
+		if _, ok := m[slot]; !ok {
+			m[slot] = []string{key}
+		} else {
+			m[slot] = append(m[slot], key)
+		}
+	}
+
+	ch := make(chan map[string][]byte)
+	max := len(m)
+	for _, keys := range m { //parrallelize calls to redis
+		go func(keys ...string) {
+			keydata, _ := this.redisbase.MGet(keys...)
+			ch <- keydata
+		}(keys...)
+	}
+
+	datamap := make(map[string][]byte)
+	var count int
+	for {
+		if count == max {
+			break
+		}
+		count++
+		chunkdata := <-ch
+		for k, v := range chunkdata {
+			datamap[k] = v
+		}
+	}
+
+	return datamap, nil
 }
