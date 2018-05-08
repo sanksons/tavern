71364a0f7312f8b29a8d273cf421c5c992351b06
Sankalp Bhatt <glitter.sankalp@gmail.com>
2018-05-08 15:58:25 +0530

    Update example of redis cluster

--- examples/rediscluster/main.go
+++ examples/rediscluster/main.go
@@ -5,8 +5,9 @@ import (
 	"log"
 	"time"
 
-	"github.com/sanksons/tavern/adapters/redis"
 	"github.com/sanksons/tavern/common/entity"
+
+	"github.com/sanksons/tavern/adapters/redis"
 )
 
 const CACHING_ENGINE = "redis-cluster"
@@ -19,17 +20,27 @@ func main() {
 	})
 
 	//This is how we set a key
-	cacheAdapter.Set(entity.CacheItem{Key: "A", Value: []byte("I am A")})
+	cacheAdapter.Set(entity.CacheItem{
+		Key:   entity.CacheKey{Name: "A"},
+		Value: []byte("I am A"),
+	})
 
 	//This is how we get a key
-	data, err := cacheAdapter.Get("A")
+	data, err := cacheAdapter.Get(entity.CacheKey{
+		Name: "A",
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("\n%s\n", data)
 
 	fmt.Println("\n get multiple Items:")
-	resultget, err := cacheAdapter.MGet("A", "B", "C", "D")
+	resultget, err := cacheAdapter.MGet(
+		[]entity.CacheKey{
+			entity.CacheKey{Name: "A"},
+			entity.CacheKey{Name: "B"},
+			entity.CacheKey{Name: "C"},
+		}...)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -38,7 +49,12 @@ func main() {
 	}
 
 	fmt.Println("\n delete Items:")
-	resultdelete, err := cacheAdapter.Destroy("A", "B", "C", "G")
+	resultdelete, err := cacheAdapter.Destroy([]entity.CacheKey{
+		entity.CacheKey{Name: "A"},
+		entity.CacheKey{Name: "B"},
+		entity.CacheKey{Name: "C"},
+		entity.CacheKey{Name: "G"},
+	}...)
 	fmt.Printf("Result: \n%+v\n", resultdelete)
 
 	//This is how we set multiple keys
@@ -49,6 +65,26 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("Result: \n%+v\n", result)
+
+	citems := prepareCacheItemswithBuckets()
+	fmt.Println("Test bucketing")
+	result, err = cacheAdapter.MSet(citems...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Result: \n%+v\n", result)
+	resultget1, err := cacheAdapter.MGet(
+		[]entity.CacheKey{
+			entity.CacheKey{Name: "AA", Bucket: "buck1"},
+			entity.CacheKey{Name: "BB", Bucket: "buck2"},
+			entity.CacheKey{Name: "CC", Bucket: "buck1"},
+		}...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for k, v := range resultget1 {
+		fmt.Printf("\n%s:%s", k, string(v))
+	}
 
 }
 
@@ -62,9 +98,28 @@ func prepareCacheItems() []entity.CacheItem {
 	cacheItems := make([]entity.CacheItem, 0)
 	for k, v := range data {
 		item := entity.CacheItem{
-			Key:        entity.CacheKey(k),
+			Key:        entity.CacheKey{Name: k},
 			Value:      []byte(v),
 			Expiration: time.Second * 2,
+		}
+		cacheItems = append(cacheItems, item)
+	}
+	return cacheItems
+}
+
+func prepareCacheItemswithBuckets() []entity.CacheItem {
+	data := map[string][]string{
+		"AA": []string{"I am A", "buck1"},
+		"BB": []string{"I am A", "buck2"},
+		"CC": []string{"I am C", "buck1"},
+		"ZZ": []string{"ZZZZZZZZZZZZZZZ", "buck3"},
+	}
+	cacheItems := make([]entity.CacheItem, 0)
+	for k, v := range data {
+		item := entity.CacheItem{
+			Key:        entity.CacheKey{Name: k, Bucket: v[1]},
+			Value:      []byte(v[0]),
+			Expiration: time.Second * 200,
 		}
 		cacheItems = append(cacheItems, item)
 	}
